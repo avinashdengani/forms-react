2686307c7e0a1f2a89c42139b64153e5d8ecc37e
Avinash Dengani <avinashdengani123@gmail.com>
2022-03-18 18:44:18 +0530

    Use refs to handle form Submit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div>
-      <h2>Hello World From React!</h2>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+
+onFormSubmit = (evt) => {
+  evt.preventDefault();
+  console.log(this.refs.name.value);
+}
+
+  render() {
+    return (
+      <div>
+        <h2>Sign Up Sheet</h2>
+        <form onSubmit={this.onFormSubmit} >
+            <input 
+                type="text"
+                placeholder="Enter name"
+                ref="name"
+            />
+            <input type="Submit" />
+        </form> 
+      </div>
+    );
+  }
 }
 
 export default App;
